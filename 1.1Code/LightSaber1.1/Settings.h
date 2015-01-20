7393739f49a15cb01ec67078b0bc87241c605dc4
alextwin007 <aalves@hmc.edu>
2015-01-19 19:43:11 -0800

    Added code for Lightsaber1.1

---
+++ 1.1Code/LightSaber1.1/Settings.h
@@ -0,0 +1,19 @@
+// Here is all the settings that are designed to be easily changed by the user.
+
+// Pins conected to the SD card reader, follows standard SPI naming conveintions
+
+#define MOSIPIN
+#define MISOPIN
+#define SXCKPIN
+#define CSPIN
+
+
+// Gyro input pins, the analog values
+#define XPIN 
+#define YPIN
+
+// Swing sense, lower is more sensitive
+#define SWINGTHRESH
+
+
+#define CLASHTHRESH
---
+++ 1.1Code/LightSaber1.1/Sound.ino
@@ -0,0 +1,57 @@
+// this file contains the code for reading and playing the audio sand
+//    from the SD card
+
+//here we globaly declare the vaiables for the library
+
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+
+// This is the name of our Wave player object
+AudioPlaySdWav           playWav1;       //xy=154,78
+
+// We are using the DAC output
+AudioOutputAnalog        dac1;           //xy=608,248
+
+// Conecting the DAC output to playWav1
+AudioConnection          patchCord1(playWav1, dac1);
+
+//TODO figure out what this is
+AudioControlSGTL5000     sgtl5000_1;     //xy=240,153
+
+void setupAudio()
+{
+  AudioMemory(5);
+  sgtl5000_1.enable();
+  sgtl5000_1.volume(1);
+  
+  // Set the SPI pins
+  SPI.setMISO(MISOPIN);
+  SPI.setMOSI(MOSIPIN);
+  SPI.setSCK(SCKPIN;
+  
+  // Conect to the SD card
+  if (!(SD.begin(10))) {
+    // stop here, but print a message repetitively
+    while (1) {
+      //TODO change to play some tone? or other messages
+      Serial.println("Unable to access the SD card");
+      delay(500);
+    }
+  }
+}
+
+
+void playFile(const char *filename)
+{
+  Serial.print("Playing file: ");
+  Serial.println(filename);
+
+  // Start playing the file.  This sketch continues to
+  // run while the file plays.
+  playWav1.play(filename);
+
+  // A brief delay for the library read WAV info
+  delay(5);
+}
